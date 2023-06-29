521885f732ea853b630f01b5a8673dafcf4bcf1c
Juan Pablo Pinzon <juanpablopinzon3717@gmail.com>
2023-06-28 21:21:58 -0500

    new functions

--- functions.js
+++ functions.js
@@ -2,8 +2,12 @@ let number = 1;
 
 function newItem() {
     var createElement = document.createElement("div");
-    var TextNewItem = document.createTextNode("Student " + number);
+    var input = document.createElement("input");
+    input.type = "text";
+    input.classList.add("type");
+    var TextNewItem = document.createTextNode("Student " + number + ": ");
     createElement.appendChild(TextNewItem);
+    createElement.appendChild(input);
     var list = document.getElementById("item");
     list.appendChild(createElement);
     number++;
@@ -11,3 +15,16 @@ function newItem() {
 
 let button = document.getElementById("button");
 button.addEventListener("click", newItem);
+
+function calculateAverage() {
+    var elementTypes = document.getElementsByClassName("type");
+    if (typeof elementTypes === "number") {
+        alert("this is a number");
+    } else {
+        alert("this is a string");
+    }
+
+}
+
+let result = document.getElementById("average");
+result.addEventListener("click", calculateAverage);
