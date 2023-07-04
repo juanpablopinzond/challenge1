4b8c3c1c0655225da08cef3f651038965df7cade
Juan Pablo Pinzon <juanpablopinzon3717@gmail.com>
2023-07-03 20:28:09 -0500

    final modifications

--- functions.js
+++ functions.js
@@ -3,7 +3,6 @@ let number = 1;
 function newItem() {
     var createElement = document.createElement("div");
     var input = document.createElement("input");
-    input.type = "text";
     input.classList.add("type");
     var TextNewItem = document.createTextNode("Student " + number + ": ");
     createElement.appendChild(TextNewItem);
@@ -16,12 +15,32 @@ function newItem() {
 let button = document.getElementById("button");
 button.addEventListener("click", newItem);
 
+
+
+
+
 function calculateAverage() {
-    var elementTypes = document.getElementsByClassName("type");
-    if (typeof elementTypes === "number") {
-        alert("this is a number");
+    var inputs = document.querySelectorAll("input");
+    var sum = 0;
+    var allnumbers = true;
+    for (var i = 0; i < inputs.length; i++) {
+        var value = parseInt(inputs[i].value);
+        if (value === "" || isNaN(value)) {
+            allnumbers = false;
+            break;
+        }
+        sum += value;
+    }
+
+    var valueResult = document.getElementById("result");
+
+    if (allnumbers && inputs.length > 0) {
+        var average = sum / inputs.length;
+        valueResult.textContent = "The average is: " + average;
+    } else if (!allnumbers) {
+        valueResult.textContent = "One of the values are not a number, please add correctly";
     } else {
-        alert("this is a string");
+        valueResult.textContent = "Please add value";
     }
 
 }
